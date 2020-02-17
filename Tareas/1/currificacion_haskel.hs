f103f70eda9d196217f28bdec3aa97b9ed06a0e4
MauricioAlv <49795209+MauricioAlv@users.noreply.github.com>
2020-02-16 22:58:23 -0500

    Create currificacion_haskel.hs
    Ejemplo de currificación en Haskell.

---
+++ Tareas/1/currificacion_haskel.hs
@@ -0,0 +1,4 @@
+-- La currificación está incluída en Haskell propiamente, gracias a ello podemos hacer cosas como la siguente:
+-- Donde los argumentos son parciales, pero se interpretan a la totalidad de los elementos. 
+map (+3) [1,7,3,2,6]
+-- Resultado: [4,10,6,5,9]
