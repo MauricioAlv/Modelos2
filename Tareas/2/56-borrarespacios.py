c9da95de2453d6503019ff58c7790d0bd700a8a9
MauricioAlv <49795209+MauricioAlv@users.noreply.github.com>
2020-03-28 13:33:23 -0500

    Create 56-borrarespacios.py
    Ejercicios Repaso. Borrar espacios en blanco.

---
+++ Tareas/2/56-borrarespacios.py
@@ -0,0 +1,11 @@
+def main():
+    # Valor recibido
+    string = input("Ingrese un texto: ")
+
+    #quitar espacios
+    # String sin Espacios para verifica palíndrome.
+    string_se= string.replace(" ", "")
+
+    print(string)
+    print(string_se)
+main()
